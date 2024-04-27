33fb535d89197762653843c3ab9ff143b87d1f2d
Davi da Costa Silva Barros <143516434+lcpdavi@users.noreply.github.com>
2024-04-26 21:00:07 -0300

    Trabalho2Poligono.c

--- Trabalho2Poligono.c
+++ Trabalho2Poligono.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
 // Definicao da struct Ponto
 typedef struct {
     double x;
     double y;
 } Ponto;
-
 // Calcular a area de um triângulo
 double AreadoTriangulo(Ponto A, Ponto B, Ponto C) {
     return fabs(0.5 * ((A.x*(B.y-C.y)) + (B.x*(C.y-A.y)) + (C.x*(A.y-B.y))));
 }
-
 // Calcular a area de um poligono
 double AreadoPoligono(Ponto* vertices, int num_vertices) {
     double area = 0.0;
@@ -21,21 +19,16 @@ double AreadoPoligono(Ponto* vertices, int num_vertices) {
     }
     return fabs(area);
 }
-
-// Funcao principal
 int main(){
     FILE *arquivo;
     int numVertices;
     Ponto *vertices;
 
-    // Abre o arquivo para escrita
     arquivo = fopen("vertices.txt", "w");
     if(arquivo == NULL){
         printf("Erro ao abrir o arquivo para escrita.");
         return 1;
     }
-
-    // Pede ao usuario o número de vertices
     printf("Coloque o numero de vertice do poligono: ");
     scanf("%d", &numVertices);
 
@@ -45,21 +38,17 @@ int main(){
         printf("Erro de alocacao de memoria.");
         return 1;
     }
-
-    // Pede ao usuario para inserir as coordenadas dos vertices e as escreve no arquivo
+    // Pede ao usuario para inserir as coordenadas dos vertices e assim escreve no arquivo
     printf("Insira as coordenadas dos vertices (formato: x y):\n");
     for(int i = 0; i < numVertices; i++){
         printf("Vertice %d: ", i + 1);
         scanf("%lf %lf", &vertices[i].x, &vertices[i].y);
         fprintf(arquivo, "%.2lf %.2lf\n", vertices[i].x, vertices[i].y);
     }
-    // Fechar o arquivo
     fclose(arquivo);
 
-    // Calcula a area do polígono
     double area = AreadoPoligono(vertices, numVertices);
 
-    // Imprime o resultado
     printf("A area do poligono e: %.2f\n", area);
 
     // Libera a memoria alocada
